8731d20dc48ce1c0a6cb5681e64332f4e966b54a
George Lymperopoulos <lympe7@hotmail.com>
2018-11-07 00:51:19 +0200

    Creation_Class_File

---
+++ Project3/File.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+
+namespace Project3
+{
+    internal class File
+    {
+        internal string Path{get; private set;}
+        // dimioutgei mia lista parts
+        private List<string[]> parts = new List<string[]>();
+        //dimiourgei mia lista apo antikeimena student
+        private List<student> studentslist = new List<student>();
+
+        internal File(string Path)
+        {
+            //perinei thn kathe grammh tou arxeio kai thn vazei se ena pinaka Lines
+            string[] Lines = System.IO.File.ReadAllLines(Path);
+            foreach (string line in Lines)
+            {
+                //Student student = new Student();
+                parts.Add(line.Split(','));
+
+            }
+            foreach (string[] part in parts)
+            {
+                student student1 = new student(part[0], part[1], part[2], part[3], part[4], part[5], part[6]);
+                studentslist.Add(student1);
+            }
+            var CompareByName = new CompareLastName();
+            studentslist.Sort(CompareByName);
+
+            foreach (student part in studentslist)
+            {
+                Console.WriteLine($"{ part.Name} | { part.LastName} | { part.Age} | { part.Height} | { part.Phone} | { part.Date} | { part.Tuition} | { part.conduct}");
+            }
+            Console.WriteLine("---------------------------------");
+
+            //sugkrish vash ilikias
+            var CompareByAge = new CompareAge();
+            studentslist.Sort(CompareByAge);
+            foreach (student part in studentslist)
+            {
+                Console.WriteLine($"{ part.Name} | { part.LastName} | { part.Age} | { part.Height} | { part.Phone} | { part.Date} | { part.Tuition} | { part.conduct}");
+            }
+            Console.WriteLine("---------------------------------");
+            //sugkrish vash phone
+            var CompareByPhone = new ComparePhone();
+            studentslist.Sort(CompareByPhone);
+            foreach (student part in studentslist)
+            {
+                Console.WriteLine($"{ part.Name} | { part.LastName} | { part.Age} | { part.Height} | { part.Phone} | { part.Date} | { part.Tuition} | { part.conduct}");
+            }
+
+        }
+
+
+    }
+}
