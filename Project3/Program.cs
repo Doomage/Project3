5847540c99fe9755661a7c7f81d55c6d486c815a
George Lymperopoulos <44376808+Doomage@users.noreply.github.com>
2018-11-20 20:23:04 +0200

    Update Program.cs

--- Project3/Program.cs
+++ Project3/Program.cs
@@ -1,2 +1,6 @@
-﻿namespace Project3 {     class MainClass     {         private const string Path = @"/Users/doomage/Desktop/test/Lab3.txt";          public static void Main(string[] args)         {
+﻿namespace Project3 
+ {    
+             class MainClass     
+             {         
+                         private const string Path = @"Path.txt";          public static void Main(string[] args)         {
             File x = new File(Path);         }     } }    
